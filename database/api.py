4d233dc909a1beb8287e1d858b7d59be6289840c
Mae-4815162342 <maelysdelouis@gmail.com>
2023-12-27 09:57:13 +0100

    database-correction

--- database/api.py
+++ database/api.py
@@ -1,11 +1,8 @@
-import requests
+from urllib.request import urlretrieve
 
-def get_file_from(url):
-  """Retrieves file from http address
+def get_file(url, filepath):
+  """Retrieves file from url address and saves it un filepath
   :param str address: the http address of the file
   :return str file: the content of the file
   :return None: if the request fails, returns None"""
-  r = requests.get(url)
-  if r.status_code == 200:
-    return r.text
-  return None
+  urlretrieve(url, filepath)
--- database/script.py
+++ database/script.py
@@ -1,6 +1,7 @@
 import os
 import api
 import parse_summary as ps
+import re
 
 # online address of NCBI database
 archea_address = "http://ftp.ncbi.nih.gov/genomes/refseq/archaea/"
@@ -17,13 +18,10 @@ if not os.path.exists(bacteria_path):
 # 1. Retrieve data summaries
 summary_file = "assembly_summary.txt"
 if not os.path.exists(bacteria_path + summary_file):
-  bacteria_summary = api.get_file_from(bacteria_address + summary_file)
-  with open(bacteria_path + summary_file, 'w', encoding="utf-8") as file:
-    file.write(bacteria_summary)
+  bacteria_summary = api.get_file(bacteria_address + summary_file, bacteria_path + summary_file)
+
 if not os.path.exists(archea_path + summary_file):
-  archea_summary = api.get_file_from(archea_address + summary_file)
-  with open(archea_path + summary_file, 'w', encoding="utf-8") as file:
-    file.write(archea_summary)
+  archea_summary = api.get_file(archea_address + summary_file, archea_path + summary_file)
 
 # 2. Get list of whole genomes
 bacteria_data = ps.read_summary(bacteria_path + summary_file)
@@ -33,15 +31,13 @@ archea_dict = ps.select_full_genome(archea_data)
 
 # 3. Retrieve .fna of each species in list
 for species in archea_dict.keys():
-  file = archea_path + species.replace(' ', '_').replace('/', '-') + '.fna.gz'
+  file = archea_path + re.sub('[^0-9a-zA-Z]+', '_', species) + '.fna.gz'
   if not os.path.exists(file):
-    data = api.get_file_from(archea_dict[species])
-    with open(file, 'w', encoding="utf-8") as f:
-      f.write(data)
+    print(f'Downloading {species}')
+    api.get_file(archea_dict[species], file)
   
 for species in bacteria_dict.keys():
-  file = bacteria_path + species.replace(' ', '_').replace('/', '-') + '.fna.gz'
+  file = bacteria_path + re.sub('[^0-9a-zA-Z]+', '_', species) + '.fna.gz'
   if not os.path.exists(file):
-    data = api.get_file_from(bacteria_dict[species])
-    with open(file, 'w', encoding="utf-8") as f:
-      f.write(data)
+    print(f'Downloading {species}')
+    api.get_file(bacteria_dict[species], file)
